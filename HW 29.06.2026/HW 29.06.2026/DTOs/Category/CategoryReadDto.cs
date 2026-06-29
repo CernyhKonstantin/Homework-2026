44344ef228a3f42c4a02f5f77c44dd898207e001
Konstantin Cernyh <kostik.cernyh@gmail.com>
2026-06-29 21:35:48 +0200

    Add files via upload

---
+++ HW 29.06.2026/HW 29.06.2026/DTOs/Category/CategoryReadDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HW_29_06_2026.DTOs.Category;
+
+public class CategoryReadDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Slug { get; set; } = string.Empty;
+    public int? ParentId { get; set; }
+}
